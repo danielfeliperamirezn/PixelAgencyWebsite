cdb9df08292908d28723598d066fc317bcbe6d38
Daniel <daniel@pixelagency.co>
2022-12-01 16:28:03 -0500

    se corrige un estilo de llamada

--- danielramirez/permanentes/footer.php
+++ danielramirez/permanentes/footer.php
@@ -1,4 +1,9 @@
 <?php ?>
 
 </body>
+<footer>
+    <div class="containerButtoCall">
+        <a href="https://call.whatsapp.com/voice/hHakz760ihFQUmvCBkREPy"><span class="icon-phone"></span> </a>
+    </div>
+</footer>
 </html>
