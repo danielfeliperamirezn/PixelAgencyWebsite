4e3122b135fde3756ca541a6e7d0105536e783c1
Daniel <daniel@pixelagency.co>
2022-12-02 11:56:10 -0500

    se sube nuevo cliente ximenachaux

---
+++ web/ximenachaux/index.php
@@ -0,0 +1,69 @@
+<?php 
+    include('../permanentes/header.php');
+?>
+
+    <section class="containerProductDetails">
+        <div class="containerHeaderProduct">
+            <figure>
+                <img src="../assets/img/Portafolio/ximenachaux/principal.webp" alt="AOF">
+            </figure>
+            <div class="contentSplas">
+                <span>Más</span>
+            </div>
+        </div>
+        <div class="containerResumenWebsite">
+            <div class="leftResumen">
+                <div>  
+                    <h3>Cliente</h3>
+                    <ul>
+                        <li>Dra Ximena Chaux</li>
+                        <li>Año</li>
+                        <li>2022</li>
+                        <li>Llano Grande - Antioquia</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Que se hizo</h3>
+                    <ul>
+                        <li><span>•</span> Diseño Web</li>
+                        <li><span>•</span> Programación</li>
+                        <li><span>•</span> Posicionamiento</li>
+                        <li><span>•</span> Estrategia digital</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="rightResumen">
+                <h2>Review</h2>
+                <p>
+                   Centro de Entrenamiento Orofacial #1 en Medellin, lider y fundador del Contox Colombia, con presencia en más de 4 paises
+                   en todo Suramerica.
+                </p>
+                <a target="_blank" href="https://draximenachauxaof.com/"> Ir al Sitio</a>
+            </div>
+        </div>
+        <div class="resumenWebsite">
+            <div class="contentResumenWebsite">
+                <h2>Landing Page</h2>
+                <p>
+                   Creamos una experiencia Inmersiva en todo el sitio web para poder mostrar de primera mano todo el contenido que tiene el centro de entrenamiento AOF
+                   resaltando las princiaples características de todo el contenido que presta a nivel nacional e internacional.
+                </p>
+            </div>
+            <div class="contentImageClient">
+                <figure>
+                    <img src="../assets/img/Portafolio/ximenachaux/1.webp" alt="Ximena Chaux">
+                </figure>
+                <figure>
+                    <img src="../assets/img/Portafolio/ximenachaux/2.webp" alt="AOF">
+                </figure>
+            </div>
+        </div>
+    </section>
+    <div class="container_next_product">
+        <a href="../catalinaVega">Próximo Cliente - <span>Catalina Vega...</span> <span class="espacio"> &#160; </span></a>
+    </div>
+
+
+<?php 
+    include("../permanentes/footer.php");
+?>
