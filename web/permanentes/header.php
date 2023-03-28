ca77451d608a9f9b129be3322db56ce9fd9a5691
Daniel <daniel@pixelagency.co>
2023-03-28 18:00:29 -0500

    se sube el meta de google Merchan

--- web/permanentes/header.php
+++ web/permanentes/header.php
@@ -18,11 +18,13 @@
         gtag('js', new Date());
 
         gtag('config', 'G-LE24LQP4HE');
+        
     </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="google-site-verification" content="IBrab_MqG7Z4FRapf0ALC66J-mqHaxVW_yjKNEFh3d8" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="google-site-verification" content="1Sbm-mtZYA3fYf4WXWdSOybnHSmCPYlgSkP7oZUZ1l8" />
     <meta name="description" content="Somos Lideres en diseño y desarrollo web y marketing digital, tiendas virtuales y posicionamiento SEO, Contáctanos.">
     <meta name="keywords" content="Diseño web medellin, marketing ditial, publicidad digital, agencia digital medellin, soluciones web, páginas web medellin, redes sociales, agencia de marketing digital, agencia de marketing, seo, seo medellin, necesito una página web, ecomerce, tiendasvirutalesmedellin">
     
